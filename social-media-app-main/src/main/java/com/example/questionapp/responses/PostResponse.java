1ca8568a62d17355c68bc4b46eff8e693d112982
Vindya Perera <vindipererauom@gmail.com>
2025-07-10 18:08:37 +1000

    Update Comments

--- social-media-app-main/src/main/java/com/example/questionapp/responses/PostResponse.java
+++ social-media-app-main/src/main/java/com/example/questionapp/responses/PostResponse.java
@@ -1,29 +1,28 @@
 package com.example.questionapp.responses;
 
-
-import com.example.questionapp.entities.Like;
-import com.example.questionapp.entities.Post;
-import lombok.Data;
-
 import java.util.List;
 
+import com.example.questionapp.entities.Post;
+
+import lombok.Data;
+
 @Data
-public class PostResponse {  //bu classı kullanmamızın sebebi password gibi bilgileri dönmek istememiz. Sadece istediğimiz bilgileri döneriz.
-    // {"id": 102,"userId": 1,"username": "ysk","text": "neden haberler","title": "haberlerim" }
+public class PostResponse {
 
-    private Long id;
-    private Long userId;
-    private String username;
-    private String text;
-    private String title;
+	private Long id;
+	private Long userId;
+	private String username;
+	private String text;
+	private String title;
 
-    private List<LikeResponse> postLikes;
-    public PostResponse(Post post, List<LikeResponse> likes){ //constructor mapping
-       this.id=post.getId();
-       this.userId=post.getUser().getId();
-       this.username=post.getUser().getUsername();
-       this.title=post.getTitle();
-       this.text=post.getText();
-       this.postLikes=likes;
-    }
+	private List<LikeResponse> postLikes;
+
+	public PostResponse(Post post, List<LikeResponse> likes) { // constructor mapping
+		this.id = post.getId();
+		this.userId = post.getUser().getId();
+		this.username = post.getUser().getUsername();
+		this.title = post.getTitle();
+		this.text = post.getText();
+		this.postLikes = likes;
+	}
 }
